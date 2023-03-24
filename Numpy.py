0bf146f58f7a2735f29b7e7ffe1e166f0324e7b9
adithyakrishnas <adithyakrishnas2015@gmail.com>
2023-03-24 23:17:53 +0530

    Numpy

---
+++ Numpy.py
@@ -0,0 +1,26 @@
+from numpy import *
+
+# arr = array([1, 2, 3, 4, 5.0], int)
+arr = array([1, 2, 3, 4, 5.0], float)
+# print(arr)
+# print(arr.dtype)
+
+
+# arr = linspace(0,15,16)
+arr = linspace(0, 9, 10)
+
+arr = arange(0, 10, 2)
+
+arr = logspace(1, 40, 5)
+
+
+
+# print(arr)
+
+# print('%.2f' %arr[0])
+# print('%.2f' %arr[4])
+
+arr = zeros(5, int)
+arr = ones(5, int)
+
+print(arr)
